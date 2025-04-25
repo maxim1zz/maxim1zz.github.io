afde290632a95b059ef5d65eb7417fc41a0b00b1
Maxim Ertler <maxim@web-vision.de>
2025-04-25 13:08:38 +0200

    change vite config to normal

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
-export default {
-  base: "maximi.zz",
-};
+export default defineConfig({
+  plugins: [react()],
+})
